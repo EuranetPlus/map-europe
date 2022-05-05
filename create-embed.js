8c51aa51ac07613bcd3c5ce27356b63acd524655
alsino <iamahorse@gmail.com>
2022-05-05 09:21:35 +0200

    map title in createEmbed.js more flexible

--- create-embed.js
+++ create-embed.js
@@ -6,11 +6,14 @@ dotenv.config()
 console.log(chalk.green('Creating iframe embed code...'));
 
 import { config } from "./src/lib/stores/config-features.js";
-const mapTitle = config.mapTitle;
+let mapTitle = config.mapTitle;
 let url = config.vercelURL;
 
+// Add https for url if non-existent
 url.includes("https://") ? url = url : url = `https://${url}`;
 
+// Replace all spaces with dashes
+mapTitle.includes(" ") ? mapTitle = mapTitle.replace(/ /g, "-").toLowerCase() : mapTitle = mapTitle;
   
 const iframe = `<iframe title="Euranet Map" aria-label="Map" id="euranet-map-${mapTitle}" src="${url}/" scrolling="no" frameborder="0"style="width: 0; min-width: 100% !important; border: none;" height="624"></iframe><script type="text/javascript">window.addEventListener("message",e=>{if("${url}"!==e.origin)return;let t=e.data;if(t.height){document.getElementById("euranet-map-${mapTitle}").height=t.height+"px"}},!1)</sc` + `ript>`
 
