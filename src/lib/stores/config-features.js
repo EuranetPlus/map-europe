4fb704e6c6a0f888c20b4006c9a0086849daf0c7
alsino <iamahorse@gmail.com>
2022-10-28 12:07:19 +0200

    added values data with extraInfo

--- src/lib/stores/config-features.js
+++ src/lib/stores/config-features.js
@@ -1,7 +1,7 @@
 export const config = {
   "mapTitle": "europe", // The title of the map, if more than one word, separate by dash, e.g. "income-europe"
   "vercelURL": "euranet-map-europe.vercel.app", // The url provided when deploying the map on Vercel
-  "datasetType": "binary", // "values" or "binary"; Does the data set contain numerical values or binary (0/1) values for countries?
+  "datasetType": "values", // "values" or "binary"; Does the data set contain numerical values or binary (0/1) values for countries?
   "datasetUnit": "percent", // "fullNumbers" or "percent"; Is the data in percent (0.25 of GDP) or full numbers (25 people)?
   "colourScheme": "blue", // one of the following: "blue", "purple-blue", "green-blue", "orange-red"
   "headlineAvailable": true, // true or false; Should the map have a headline? 
