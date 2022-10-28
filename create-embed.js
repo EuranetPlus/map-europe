8d5522bcd41ee1bac0113a5e8e62815fdded97c4
alsino <iamahorse@gmail.com>
2022-10-28 11:54:20 +0200

    fix embed-generator to account for trailing slages in vercel url

--- create-embed.js
+++ create-embed.js
@@ -12,10 +12,14 @@ let url = config.vercelURL;
 // Add https for url if non-existent
 url.includes("https://") ? url = url : url = `https://${url}`;
 
+// Remove trailing slash in url
+url.includes("vercel.app/") ? url = url.replace("vercel.app/", "vercel.app" ) : url = url;
+
 // Replace all spaces with dashes
 mapTitle.includes(" ") ? mapTitle = mapTitle.replace(/ /g, "-").toLowerCase() : mapTitle = mapTitle;
   
-const iframe = `<iframe title="Euranet Map" aria-label="Map" id="euranet-map-${mapTitle}" src="${url}/" scrolling="no" frameborder="0"style="width: 0; min-width: 100% !important; border: none;" height="624"></iframe><script type="text/javascript">window.addEventListener("message",e=>{if("${url}"!==e.origin)return;let t=e.data;if(t.height){document.getElementById("euranet-map-${mapTitle}").height=t.height+"px"}},!1)</sc` + `ript>`
+let iframe = `<iframe title="Euranet Map" aria-label="Map" id="euranet-map-${mapTitle}" src="${url}" scrolling="no" frameborder="0"style="width: 0; min-width: 100% !important; border: none;" height="624"></iframe><script type="text/javascript">window.addEventListener("message",e=>{if("${url}"!==e.origin)return;let t=e.data;if(t.height){document.getElementById("euranet-map-${mapTitle}").height=t.height+"px"}},!1)</sc` + `ript>`
+
 
 let heading = "## Embedding the map";
 let subheading = "To embed the map on any website as a responsive widget, please use the following **iframe code**:"
